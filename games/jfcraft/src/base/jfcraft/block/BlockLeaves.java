e5904a06a0411c6d765183f87b21522d2f253f88
Peter Quiring <pquiring@gmail.com>
2015-08-01 10:17:45 -0400

    leaves decay

--- games/jfcraft/src/base/jfcraft/block/BlockLeaves.java
+++ games/jfcraft/src/base/jfcraft/block/BlockLeaves.java
@@ -6,16 +6,40 @@ package jfcraft.block;
  */
 
 import jfcraft.data.*;
-import static jfcraft.data.Direction.*;
 
 public class BlockLeaves extends BlockOpaqueVarPerf {
   public BlockLeaves(String name, String names[], String textures[], String textures2[]) {
     super(name, names, textures, textures2);
     canSpawnOn = false;  //stop animals from spawning on trees
     absorbLight = 1;
+    isDirFace = true;
   }
   public boolean canSupportBlock(Coords c) {
     return (c.block.id == Blocks.SNOW);
   }
-  //TODO : decay leaves
+
+  private static Coords supportingBlock = new Coords();
+  private static Coords thisBlock = new Coords();
+
+  public void tick(Chunk chunk, Tick tick) {
+    tick.toWorldCoords(chunk, thisBlock);
+    if (tick.t1 > 0) {
+      tick.t1++;
+      if (tick.t1 == 5) {
+        destroy(null, thisBlock, true);
+        chunk.delTick(tick);
+      }
+      return;
+    }
+    Static.server.world.getBlock(chunk.dim, thisBlock.x, thisBlock.y, thisBlock.z, thisBlock);
+    supportingBlock.copy(thisBlock);
+    supportingBlock.adjacentBlock();
+    Static.server.world.getBlock(thisBlock.chunk.dim, supportingBlock.x, supportingBlock.y, supportingBlock.z, supportingBlock);
+    char support_id = supportingBlock.block.id;
+    if (support_id != Blocks.WOOD && support_id != Blocks.LEAVES) {
+      tick.t1 = 1;
+    } else {
+      chunk.delTick(tick);
+    }
+  }
 }
