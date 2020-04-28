3746fa54adddfee2f97c2c635f8c8b8f030b38d1
Peter Quiring <pquiring@gmail.com>
2020-04-28 19:48:20 -0400

    fix axe recipe

--- src/base/jfcraft/recipe/RecipeAxe.java
+++ src/base/jfcraft/recipe/RecipeAxe.java
@@ -12,21 +12,26 @@ import jfcraft.data.*;
 
 public class RecipeAxe extends Recipe {
   public RecipeAxe() {
-    super(3,3);
+    super(2,3);
   }
 
   public Item make(Item items[]) {
     char id = items[0].id;
     if (items[1].id != id) return null;
-    if (items[2].id != id) return null;
-    if (items[3].id != id) return null;
 
-    if (items[4].id != Items.STICK) return null;
-    if (items[5].id != Blocks.AIR) return null;
-
-    if (items[6].id != Blocks.AIR) return null;
-    if (items[7].id != Items.STICK) return null;
-    if (items[8].id != Blocks.AIR) return null;
+    if (items[2].id == id) {
+      //sticks on right side
+      if (items[3].id != Items.STICK) return null;
+      if (items[4].id != Blocks.AIR) return null;
+      if (items[5].id != Items.STICK) return null;
+    } else if (items[3].id == id) {
+      //sticks on left side
+      if (items[2].id != Items.STICK) return null;
+      if (items[4].id != Items.STICK) return null;
+      if (items[5].id != Blocks.AIR) return null;
+    } else {
+      return null;
+    }
 
     if (id == Items.DIAMOND) return new Item(Items.DIAMOND_AXE, 1.0f);
     if (id == Items.GOLD_INGOT) return new Item(Items.GOLD_AXE, 1.0f);
