0aa86c0200b696e6c002b6ac30b2df495ce6afce
Peter Quiring <pquiring@gmail.com>
2021-10-12 13:56:32 -0400

    ModelConvert -> model.Convert

--- blender/convert.bat
+++ blender/convert.bat
@@ -1,2 +1,2 @@
 @echo off
-java -cp ..\javaforce.jar javaforce.gl.ModelConvert . ..\jfassets\assets\minecraft\models
+java -cp ..\javaforce.jar javaforce.gl.model.Convert . ..\jfassets\assets\minecraft\models
