a8b27f470a66664b4c71f182440b7714ce737783
Peter Quiring <pquiring@gmail.com>
2024-10-24 20:06:48 -0400

    fix Voxel

--- src/base/jfcraft/opengl/Voxel.java
+++ src/base/jfcraft/opengl/Voxel.java
@@ -22,14 +22,17 @@ public class Voxel implements RenderSource {
     this.var = var;
   }
 
+  private static final float dark = 0.1f;
+
   public void buildBuffers(RenderDest dest) {
     SubTexture st = item.textures[var];
     int w = st.ai.w;
     int h = st.ai.h;
-    int px[] = st.ai.getPixels();
+    int pxs[] = st.ai.getPixels();
     RenderBuffers bufs = dest.getBuffers(0);
     bufs.reset();
     float d = 1.0f / ((float)w);  //size of each voxel
+    float d2 = d / 2.0f;
     //bottom to top, left to right
     float x1 = 0f;
     float x2 = 1f;
@@ -37,52 +40,52 @@ public class Voxel implements RenderSource {
     float y2 = y1 + h * d;
     int off = w * (h-1);
     int x = 0, y = h-1;
-    float zz = 0.5f;
-    float d2 = Static._1_32;  //depth of voxels
+    float z1 = 0.5f + d2;
+    float z2 = 0.5f - d2;
     SubTexture solidTexture = Static.blocks.solid.textures[0];
     float u1 = solidTexture.x1;
     float v1 = solidTexture.y1;
     float u2 = solidTexture.x2;
     float v2 = solidTexture.y2;
     float clr[] = new float[3];
-    float clr2[] = new float[3];
     Static.data.reset();
+    Static.data.clr = clr;
     for(float fy = y1; fy < y2; fy+=d) {
       x = 0;
       for(float fx = x1; fx < x2; fx+=d) {
-        if ((px[off] & 0xff000000) == 0) {  //transparent pixel?
+        if ((pxs[off] & 0xff000000) == 0) {  //transparent pixel?
           x++;
           off++;
           continue;
         }
-        Static.data.clr = clr;
-        clr[0] = (float)((px[off] & 0xff0000) >> 16) / 255.0f;
-        clr[1] = (float)((px[off] & 0xff00) >> 8) / 255.0f;
-        clr[2] = (float)((px[off] & 0xff)) / 255.0f;
-        clr2[0] = (clr[0] > 5) ? clr[0] - 5f : 0f;
-        clr2[1] = (clr[1] > 5) ? clr[1] - 5f : 0f;
-        clr2[2] = (clr[2] > 5) ? clr[2] - 5f : 0f;
+        int px = pxs[off];
+        clr[0] = (float)((px & 0xff0000) >> 16) / 255.0f;
+        clr[1] = (float)((px & 0xff00) >> 8) / 255.0f;
+        clr[2] = (float)((px & 0xff)) / 255.0f;
         //see bufs.addBox()
         //S
-        bufs.addFace(fx,fy,zz-d2, fx+d,fy+d,zz-d2, u1,v1, u2,v2);
+        bufs.addFace(fx,fy,z2, fx+d,fy+d,z2, u1,v1, u2,v2);
         //N
-        bufs.addFace(fx+d,fy,zz+d2, fx,fy+d,zz+d2, u1,v1, u2,v2);
-        Static.data.clr = clr2;
+        bufs.addFace(fx+d,fy,z1, fx,fy+d,z1, u1,v1, u2,v2);
+        //adjust clr for sides (darker)
+        clr[0] = (clr[0] > dark) ? clr[0] - dark : 0f;
+        clr[1] = (clr[1] > dark) ? clr[1] - dark : 0f;
+        clr[2] = (clr[2] > dark) ? clr[2] - dark : 0f;
         //B
-        if (y < h-1 && (px[off + w] & 0xff000000) == 0) {
-          bufs.addFaceAB(fx,fy+d,zz+d2, fx+d,fy+d,zz-d2, u1,v1, u2,v2);
+        if (y == h-1 || (pxs[off + w] & 0xff000000) == 0) {
+          bufs.addFaceAB(fx,fy,z1, fx+d,fy,z2, u1,v1, u2,v2);
         }
         //A
-        if (y > 0 && (px[off - w] & 0xff000000) == 0) {
-          bufs.addFaceAB(fx,fy,zz-d2, fx+d,fy,zz+d2, u1,v1, u2,v2);
+        if (y == 0 || (pxs[off - w] & 0xff000000) == 0) {
+          bufs.addFaceAB(fx,fy+d,z2, fx+d,fy+d,z1, u1,v1, u2,v2);
         }
         //E
-        if (x < w-1 && (px[off + 1] & 0xff000000) == 0) {
-          bufs.addFace(fx,fy,zz-d2, fx,fy+d,zz+d2, u1,v1, u2,v2);
+        if (x == w-1 || (pxs[off + 1] & 0xff000000) == 0) {
+          bufs.addFace(fx+d,fy,z2, fx+d,fy+d,z1, u1,v1, u2,v2);
         }
         //W
-        if (x > 0 && (px[off - 1] & 0xff000000) == 0) {
-          bufs.addFace(fx+d,fy,zz+d2, fx+d,fy+d,zz-d2, u1,v1, u2,v2);
+        if (x == 0 || (pxs[off - 1] & 0xff000000) == 0) {
+          bufs.addFace(fx,fy,z1, fx,fy+d,z2, u1,v1, u2,v2);
         }
         x++;
         off++;
