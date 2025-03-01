386bc54489528acd1f11a35fab03700b607fbd30
Peter Quiring <pquiring@gmail.com>
2025-03-01 13:37:22 -0500

    increase odds of flowers in plains and forest

--- src/jfcraft/biome/BiomeForest.java
+++ src/jfcraft/biome/BiomeForest.java
@@ -16,17 +16,22 @@ public class BiomeForest extends BiomeBase {
   }
 
   private static final int TREE_ODDS = 50;
-  private static final int FLOWER_CHUNK_ODDS = 100;
-  private static final int FLOWER_BLOCK_ODDS = 100;
-  private static final int TALLGRASS_CHUNK_ODDS = 100;
-  private static final int TALLGRASS_BLOCK_ODDS = 100;
-  private static final int ANIMAL_CHUNK_ODDS = 100;
-  private static final int ANIMAL_BLOCK_ODDS = 100;
+  private static final int WEEDS_ODDS = 30;
+  private static final int FLOWER_CHUNK_ODDS = 9;
+  private static final int FLOWER_BLOCK_ODDS = 20;
+  private static final int TALLGRASS_CHUNK_ODDS = 5;
+  private static final int TALLGRASS_BLOCK_ODDS = 30;
+  private static final int ANIMAL_CHUNK_ODDS = 10;
+  private static final int ANIMAL_BLOCK_ODDS = 30;
 
   public void build(int x,int y,int z, BiomeData data) {
     if (canPlantOn(x, y, z)) {
       if (data.b1 % TREE_ODDS == 0) {
         getTree(data.b2).plant(x, y+1, z, data);
+        return;
+      }
+      if (data.b2 % WEEDS_ODDS == 0) {
+        setBlock(x,y+1,z,Blocks.WEEDS,0,0);
         return;
       }
       if (data.c1 % FLOWER_CHUNK_ODDS == 0) {
